3e119e105cabb39fdff6dbc7c009bdf5ed24bc99
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-02 23:05:57 +0100

    add player class, show board, play square

--- noughtcross.rb
+++ noughtcross.rb
@@ -6,10 +6,10 @@ class Square
 end
 
 class Row
-	attr_accessor :cells
+	attr_accessor :squares
 	def initialize
-		@cells = []
-		(0..2).each {@cells.push Square.new}
+		@squares = []
+		(0..2).each {@squares.push Square.new}
 	end
 end
 
@@ -21,11 +21,55 @@ class Board
 	end
 end
 
-class Game
+class Player 
+	attr_accessor :name, :score, :token
+	def initialize (name)
+		@name = name
+		@score = 0
+		@token = @name[0].upcase
+	end
+end
 
-	def initialize
+
+class Game
+	def initialize (player1, player2)
 		@board = Board.new
+		@player1, @player2 = player1, player2
+
 	end
 
+	def play (row, column, player)
+		if @board.rows[row].squares[column].status == :blank 
+		   @board.rows[row].squares[column].status = player
+		else
+		  return 'play again'
+		end
+	end
+
+	def show_board
+		@board.rows.each do |row|
+			puts "-------"
+			print "|"
+			row.squares.each do |square|
+				if square.status == :blank 
+					print '-'
+				else
+					print square.status.token
+				end
+				print '|'
+			end
+			puts ''
+		end
+		puts "-------"
+	end
 end
+
+mike = Player.new ('mike')
+bob = Player.new ('bob')
+
+my_game = Game.new bob, mike
+my_game.show_board
+
+my_game.play 1,1,bob
+my_game.show_board
 
