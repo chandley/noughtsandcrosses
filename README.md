4e67515695cfa5860b61bfe222699db2c3fd26e9
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-06 10:48:32 +0100

    update README, tweak comments

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Simple noughts and crosses exercise
 
+Two players take turns to enter moves at the keyboard
+
+I'd appreciate any comments on class structure, or use of array methods in this excercise.
--- noughtcross.rb
+++ noughtcross.rb
@@ -52,35 +52,10 @@ class Board
 		end
 	end
 
-	def check_3_in_row_old
-		squares.each do |row| # check rows
-			winner = check_line_winner(*row) 
-			return winner unless winner.nil?
-		end
-		(0..squares.length-1).each do |column| # check columns
-			col_squares = []
-			squares.each {|row| col_squares.push row[column]}
-			winner = check_line_winner(*col_squares)
-			return winner unless winner.nil?
-		end
-		
-		if squares.length % 2 == 1 #  diagonals exist if play size is odd
-			diagonal_down, diagonal_up = [],[]
-			(0..squares.length-1).each do |i|
-				diagonal_down.push squares[i][i]
-				diagonal_up.push squares[squares.length-1-i][i]				
-			end
-			winner = check_line_winner(*diagonal_down) || check_line_winner(*diagonal_up)
-			return winner unless winner.nil?
-		end
-
-		return nil	# no winner
-
-	end
-
 	def check_3_in_row
-		check_array = [] # don't really need to create each time
-		squares.each {|row| check_array.push row}
+		check_array = [] # don't really need to create each time - move to initialize?
+
+		squares.each {|row| check_array.push row} # check rows
 	
 		(0..squares.length-1).each do |column| # check columns
 			col_squares = []
@@ -97,7 +72,7 @@ class Board
 			check_array.push diagonal_up,	diagonal_down
 		end
 
-		check_array.each do |line|	
+		check_array.each do |line|	# check each potential winning line for winner
 			 winner = check_line_winner(*line) 
 			 return winner unless winner.nil?
 		end
